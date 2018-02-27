1cad2757598c0ed0fb39411bc2ddddad0264e9bd
Antonella Morittu <morittuantonella@gmail.com>
2018-02-27 17:19:55 +0100

    text on home_page displayed and test goes green

--- features/application.feature
+++ features/application.feature
@@ -6,5 +6,4 @@ I should see a home page
 
 Scenario: Display the home page visiting the page
   When I visit the page
-  Then I should be on the home page
-  And I should see "Welcome to Broke No More"
+  And I should see "Welcome to Broke No More!"
--- features/step_definitions/application_steps.rb
+++ features/step_definitions/application_steps.rb
@@ -2,10 +2,6 @@ When("I visit the page") do
   visit root_path
 end
 
-Then("I should be on the home page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
 Then("I should see {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content string
 end
