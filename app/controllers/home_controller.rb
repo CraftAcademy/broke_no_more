364e051885a835e3ad66c227772dff7d9a3c8987
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-28 18:18:46 +0100

    WIP user enters monthly income create routes for home controller set submit form on index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def create
+    binding.pry
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :home, only: [:create, :index, :update]
+
   root controller: :home, action: :index
+
+  root 'home#index'
 end
