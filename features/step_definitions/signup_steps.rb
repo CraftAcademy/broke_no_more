1787be3a32c13c522205f92fd1400e4248e7815f
Antonella Morittu <morittuantonella@gmail.com>
2018-02-28 16:35:39 +0100

    create signup feature create navbar in layouts

---
+++ features/step_definitions/signup_steps.rb
@@ -0,0 +1,19 @@
+Given("user visit the site") do
+  visit root_path
+end
+
+Then("user click on {string} button") do |signup|
+  click_link_or_button signup
+end
+
+Then("He fills in {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("He clicks {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("He sees message {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
