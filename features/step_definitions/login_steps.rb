49b0a4929d5be00a665f522f4a4d18dd468298a3
FleurTamboezer <ftamboezer@gmail.com>
2018-03-01 15:07:28 +0100

    First login step passes

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,29 @@
+Given("following user exist") do |table|
+ table.hashes.each do |user|
+   FactoryBot.create(:user, user)
+ end
+end
+
+Given("I am on the landing-page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I click {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I fill in {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am logged in") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("show me the page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
