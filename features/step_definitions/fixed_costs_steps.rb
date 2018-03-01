4fd08e9cca50b794aefc8e7ebe1fae7b4b926ae6
Isabelle <isabellelidberg@live.se>
2018-03-01 16:46:04 +0100

    Add snippets to steps

---
+++ features/step_definitions/fixed_costs_steps.rb
@@ -0,0 +1,15 @@
+Given("I am on home page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see {string} form") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I fill in {string} field with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I click {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
