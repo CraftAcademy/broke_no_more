0449cf5d4bb59d9892dba71780ba37840d6653dc
Isabelle <isabellelidberg@live.se>
2018-03-02 11:03:46 +0100

    Create rspec tests

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-
+  describe 'DB table' do
+    it { should have_db_column :rent }
+    it { should have_db_column :transportation }
+    it { should have_db_column :education }
+    it { should have_db_column :food }
+    it { should have_db_column :other }
+    it { should have_db_column :insurance }
+  end
 end
