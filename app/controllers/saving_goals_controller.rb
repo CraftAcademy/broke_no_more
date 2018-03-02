94b0b044eeca9cbe4cf0ab5e40d763c514851e4c
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-03-02 14:43:25 +0100

    add flash message for saving_goal

--- app/controllers/saving_goals_controller.rb
+++ app/controllers/saving_goals_controller.rb
@@ -3,6 +3,20 @@ class SavingGoalsController < ApplicationController
   end
 
   def create
+    @saving_goal = SavingGoal.new(saving_goal_params)
+    if @saving_goal.save
+      flash[:success] = "Savings Goal saved"
+      redirect_to home_index_path
+    else
+      flash[:error] = "Please enter valid amount"
+      render 'new'
+    end
   end
 
+
+  private
+
+  def saving_goal_params
+    params.require(:saving_goal).permit(:amount)
+  end
 end
