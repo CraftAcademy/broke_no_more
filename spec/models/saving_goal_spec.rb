1662095e4a31def3fbdba7c01d45b51b34cbcf55
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-03-02 14:56:09 +0100

    add spec files, all green
    Co-authored-by: Fleurtam <ftamboezer@gmail.com>

--- spec/models/saving_goal_spec.rb
+++ spec/models/saving_goal_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe SavingGoal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_db_column :amount}
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryBot.create(:income)).to be_valid
+    end
+  end
 end
