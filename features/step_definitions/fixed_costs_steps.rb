1d4b1ecedbfc2652698c5ca7ffa269c941d505d2
Antonella Morittu <morittuantonella@gmail.com>
2018-03-04 13:00:41 +0100

    clean up flash messages conflict user logged in test goes green configure warden

--- features/step_definitions/fixed_costs_steps.rb
+++ features/step_definitions/fixed_costs_steps.rb
@@ -9,3 +9,8 @@ end
 Then("I fill in {string} field with {string}") do |input, value|
   fill_in input, with: value
 end
+
+Given("{string} is logged in") do |email|
+  user = User.find_by(email: email)
+  login_as(user, scope: :user)
+end
